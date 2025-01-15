5b290f230dac0dec8e6e383df034905b038a814f
asad406 <asadul76@gmail.com>
2025-01-15 08:07:26 +0600

    Role base sidebar and reuseable route created

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,64 +1,12 @@
-import { Layout, Menu, } from "antd";
-import {  Outlet } from "react-router-dom";
-import { adminSidebarItems } from "../../routes/admin.routes";
-const { Header, Content, Footer, Sider } = Layout;
-
-// const items: MenuProps["items"] = [
-//   {
-//     key: "1",
-//     label: "Dashboard",
-//   },
-//   {
-//     key: "2",
-//     label: "Dashboard",
-//     children: [
-//       {
-//         key: "11",
-//         label: <NavLink to='create-admin'>Create Admin</NavLink>,
-//       },
-//       {
-//         key: "12",
-//         label: <NavLink to='create-faculty'>Create Faculty</NavLink>,
-//       },
-//       {
-//         key: "13",
-//         label: <NavLink to='create-student'>Create Student</NavLink>,
-//       },
-//     ],
-//   },
-// ];
+import { Layout } from "antd";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+const { Header, Content } = Layout;
 
 const MainLayout = () => {
   return (
     <Layout>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div
-          style={{
-            color: "white",
-            height: "4rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <h3>PH-University</h3>
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={adminSidebarItems}
-        />
-      </Sider>
+      <Sidebar />
       <Layout style={{ height: "100vh" }}>
         <Header style={{ padding: 0 }} />
         <Content style={{ margin: "24px 16px 0" }}>
@@ -71,9 +19,6 @@ const MainLayout = () => {
             <Outlet />
           </div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
       </Layout>
     </Layout>
   );
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../pages/Login";
-import { adminRoutes } from "./admin.routes";
+import { routeGenerator } from "../utils/routesGenerator";
+import { adminPaths } from "./admin.routes";
+import { facultyPaths } from "./faculty.routes";
+import { studentPaths } from "./student.route";
 
 const router = createBrowserRouter([
     {
@@ -11,7 +14,17 @@ const router = createBrowserRouter([
     {
         path: '/admin',
         element: <App/>,
-        children: adminRoutes
+        children: routeGenerator(adminPaths)
+    },
+    {
+        path: '/faculty',
+        element: <App/>,
+        children: routeGenerator(facultyPaths)
+    },
+    {
+        path: '/student',
+        element: <App/>,
+        children: routeGenerator(studentPaths)
     },
     {
         path: '/login',
